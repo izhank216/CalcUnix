e7e890f94f8a0120e6eb311529f1834018b3e7b8
izhank216 <izhank216@gmail.com>
2026-01-07 16:31:05 +0500

    Create Calculate.h

---
+++ src/Calculate.h
@@ -0,0 +1,11 @@
+#ifndef CALCULATE_H
+#define CALCULATE_H
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+double modulo(double a, double b);
+int compare(double a, double b);
+char *concat(const char *a, const char *b);
+void reference_op(const char *op);
+#endif
