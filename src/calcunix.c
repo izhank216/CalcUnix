29d750ad338ee5a8c4c67db44c1b6d1fdbfe095c
izhank216 <izhank216@gmail.com>
2026-01-09 19:55:24 +0500

    Update calcunix.c

--- src/calcunix.c
+++ src/calcunix.c
@@ -18,42 +18,53 @@ int main() {
     char op[2];
     char input1[256], input2[256];
 
-    printf("Enter calculation (e.g., 5 + 3 or text & text): ");
+    while (1) {
+        printf("Enter calculation (e.g., 5 + 3 or text & text): ");
 
-    if (scanf("%s %1s %s", input1, op, input2) != 3) {
-        printf("Invalid input.\n");
-        return 1;
-    }
+        if (scanf("%s", input1) != 1) {
+            printf("Invalid input.\n");
+            continue;
+        }
 
-    if (op[0] == '+' || op[0] == '-' || op[0] == '*' || op[0] == '/' || op[0] == '%') {
-        a = atof(input1);
-        b = atof(input2);
-        if (op[0] == '+') result = add(a,b);
-        else if (op[0] == '-') result = subtract(a,b);
-        else if (op[0] == '*') result = multiply(a,b);
-        else if (op[0] == '/') result = divide(a,b);
-        else if (op[0] == '%') result = modulo(a,b);
-        printf("Result: %.2lf\n", result);
-    }
-    else if (op[0] == '=' || op[0] == '<' || op[0] == '>') {
-        a = atof(input1);
-        b = atof(input2);
-        int cmp = compare(a,b);
-        if (op[0] == '=') printf("Result: %s\n", (cmp==0)?"true":"false");
-        else if (op[0] == '<') printf("Result: %s\n", (cmp==-1)?"true":"false");
-        else if (op[0] == '>') printf("Result: %s\n", (cmp==1)?"true":"false");
-    }
-    else if (op[0] == '&') {
-        char *res = concat(input1, input2);
-        printf("Result: %s\n", res);
-        free(res);
-    }
-    else if (op[0]==':' || op[0]=='!' || op[0]==';') {
-        reference_op(op);
-    }
-    else {
-        printf("Unknown operator '%s'\n", op);
-        return 1;
+        if (scanf("%1s %s", op, input2) != 2) {
+            printf("Invalid input.\n");
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+            continue;
+        }
+
+        if (op[0] == '+' || op[0] == '-' || op[0] == '*' || op[0] == '/' || op[0] == '%') {
+            a = atof(input1);
+            b = atof(input2);
+            if (op[0] == '+') result = add(a,b);
+            else if (op[0] == '-') result = subtract(a,b);
+            else if (op[0] == '*') result = multiply(a,b);
+            else if (op[0] == '/') result = divide(a,b);
+            else if (op[0] == '%') result = modulo(a,b);
+            printf("Result: %.2lf\n\n", result);
+        }
+        else if (op[0] == '=' || op[0] == '<' || op[0] == '>') {
+            a = atof(input1);
+            b = atof(input2);
+            int cmp = compare(a,b);
+            if (op[0] == '=') printf("Result: %s\n\n", (cmp==0)?"true":"false");
+            else if (op[0] == '<') printf("Result: %s\n\n", (cmp==-1)?"true":"false");
+            else if (op[0] == '>') printf("Result: %s\n\n", (cmp==1)?"true":"false");
+        }
+        else if (op[0] == '&') {
+            char *res = concat(input1, input2);
+            printf("Result: %s\n\n", res);
+            free(res);
+        }
+        else if (op[0]==':' || op[0]=='!' || op[0]==';') {
+            reference_op(op);
+        }
+        else {
+            printf("Unknown operator '%s'\n\n", op);
+        }
+
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
     }
 
     return 0;
